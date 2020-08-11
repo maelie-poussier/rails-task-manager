3631a648614f119abf9c6c830b9dcbbf7ba55210
maelie poussier <maelie.poussier@icloud.com>
2020-08-11 15:03:22 +0200

    rails task manager

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,40 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @tasks = Task.all
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(params_task)
+    @task.save
+    redirect_to tasks_path(@tasks)
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(params_task)
+    redirect_to tasks_path(@tasks)
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path(@tasks)
+  end
+
+  def params_task
+    params.require(:task).permit(:title, :details)
+  end
+end
